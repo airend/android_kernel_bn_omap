5ebdf71455da0dbf20ce0a1ee70c9a934efe1a4b
Pavankumar Kondeti <pkondeti@codeaurora.org>
2012-10-09 17:47:44 +0530

    USB: android: Don't disable configuration for every adb close
    Certain ADB commands like "adb usb" and "adb root" restart the
    adb daemon running on the device.  These commands require USB
    bus reset, otherwise adb commands on the host side would stuck
    for ever.
    
    USB bus reset can be forced from device side by re-enabling the
    same composition.  Earlier, configuration is disabled when adb
    daemon closes /dev/android_usb device file.  The configuration
    is enabled again after adb daemon opens the device file. This
    approach solves the "adb usb" problem with the below side effects.
    
    1. When USB cable is disconnected, the Rx request is flushed and
    an error code is returned to user space.  The adb daemon is simply
    restarted in this case which cause an unnecessary rebinding of
    all functions.
    2. The above scenario can happen if host reset the bus when device
    is in configured state.  If device disable pull-up during, bus
    reset, USB CV CH9 test cases are failing.
    
    The commit d9b1897 (USB: android: Avoid re-enabling the same
    configuration) fixed the above side effects. But silently ignores the
    original "adb usb" problem.
    
    This commit is another attempt to solve all the above mentioned problems
    by forcing re-enumeration only when device is in connected state.  That
    means, "adb usb" and "adb root" commands force usb bus reset where as
    plugging out a cable does not.
    
    CRs-Fixed: 407882
    Change-Id: I45340221f39265df4bca4d770e613dab6345f6b6
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- drivers/usb/gadget/f_adb.c
+++ drivers/usb/gadget/f_adb.c
@@ -56,6 +56,7 @@ struct adb_dev {
 	wait_queue_head_t write_wq;
 	struct usb_request *rx_req;
 	int rx_done;
+	bool notify_close;
 };
 
 static struct usb_interface_descriptor adb_interface_desc = {
@@ -476,8 +477,10 @@ static int adb_open(struct inode *ip, struct file *fp)
 	/* clear the error latch */
 	_adb_dev->error = 0;
 
-	adb_ready_callback();
+	if (_adb_dev->notify_close)
+		adb_ready_callback();
 
+	_adb_dev->notify_close = true;
 	return 0;
 }
 
@@ -485,7 +488,16 @@ static int adb_release(struct inode *ip, struct file *fp)
 {
 	pr_info("adb_release\n");
 
-	adb_closed_callback();
+	/*
+	 * ADB daemon closes the device file after I/O error.  The
+	 * I/O error happen when Rx requests are flushed during
+	 * cable disconnect or bus reset in configured state.  Disabling
+	 * USB configuration and pull-up during these scenarios are
+	 * undesired.  We want to force bus reset only for certain
+	 * commands like "adb root" and "adb usb".
+	 */
+	if (_adb_dev->notify_close)
+		adb_closed_callback();
 
 	adb_unlock(&_adb_dev->open_excl);
 	return 0;
@@ -613,6 +625,12 @@ static void adb_function_disable(struct usb_function *f)
 	struct usb_composite_dev	*cdev = dev->cdev;
 
 	DBG(cdev, "adb_function_disable cdev %p\n", cdev);
+	/*
+	 * Bus reset happened or cable disconnected.  No
+	 * need to disable the configuration now.  We will
+	 * set noify_close to true when device file is re-opened.
+	 */
+	dev->notify_close = false;
 	dev->online = 0;
 	dev->error = 1;
 	usb_ep_disable(dev->ep_in);
@@ -661,6 +679,7 @@ static int adb_setup(void)
 	atomic_set(&dev->open_excl, 0);
 	atomic_set(&dev->read_excl, 0);
 	atomic_set(&dev->write_excl, 0);
+	dev->notify_close = true;
 
 	INIT_LIST_HEAD(&dev->tx_idle);
 
