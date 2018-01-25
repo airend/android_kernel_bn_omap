5da01dce91037cb36b12c7eba0db3c5fff81fcd3
D. Andrei Măceș <dmaces@nd.edu>
2018-01-25 17:21:10 +0200

    omaplfb: Prepare for modular build (omaplfb.ko)
    Change-Id: If03c1c9c4dbf1bef180a42ed497fd2bd7ec7599b
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- drivers/video/omap2/omaplfb/omaplfb_displayclass.c
+++ drivers/video/omap2/omaplfb/omaplfb_displayclass.c
@@ -1737,7 +1737,7 @@ ErrorExit:
 	return NULL;
 }
 
-#if defined(CONFIG_OMAPLFB)
+#if defined(CONFIG_OMAPLFB) || defined(CONFIG_OMAPLFB_MODULE)
 int OMAPLFBRegisterPVRDriver(PFN_DC_GET_PVRJTABLE pfnFuncTable)
 {
 	gpfnGetPVRJTable = pfnFuncTable;
@@ -1759,7 +1759,7 @@ OMAPLFB_ERROR OMAPLFBInit(void)
 	unsigned i;
 	unsigned uiDevicesFound = 0;
 
-#if !defined(CONFIG_OMAPLFB)
+#if !(defined(CONFIG_OMAPLFB) || defined(CONFIG_OMAPLFB_MODULE))
 	if(OMAPLFBGetLibFuncAddr ("PVRGetDisplayClassJTable", &gpfnGetPVRJTable) != OMAPLFB_OK)
 	{
 		return OMAPLFB_ERROR_INIT_FAILURE;
